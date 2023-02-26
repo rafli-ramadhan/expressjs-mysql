33c17e6b3d47264ae7bab2bbe22757e51358c4f3
Mochamad Rafli Ramadhan <rafliugm222@gmail.com>
2023-02-26 17:28:30 +0700

    refactor: update readme and routes

--- README.md
+++ README.md
@@ -1,41 +1,26 @@
-## Express (4.17.3) and Sequelize (6.16.1) - MySQL with @Hapi/Joi Validator for CRUD and Auth Application
+# Express + Sequelize + MySQL with @Hapi/Joi (CRUD and Auth Application)
 
-### Requirement
+## Requirement
 
 1. Node.js
 2. MySQL
-3. Apache
-4. Postman
 
-### Set up
+## Set Up :rocket:
 
 ```
 npm install
 ```
 
-### Running for Development
 ```
 npm run dev
 ```
 
 App will be running on port 5000 (http://localhost:5000).
 
-### What is this Repository for?
-
-This project was my undergraduate thesis for chatbot backend that build with express.js and sequelize with mysql connection.
-
-### Clone this Repository ?
-
-Feel free to clone this repo.
-
-### API Endpoint
-
-Open `./routes/routes.js`
-
-### API Endpoint for Development 
+## API Endpoint for Development 
 
 http://127.0.0.1:5000
 
-### Important Note 
+## Reference 
 
 This code was originally cloned from https://github.com/trulymittal/
--- Routes/index.js
+++ Routes/index.js
@@ -1,24 +1,16 @@
 module.exports = app => {
   const router = require("express").Router();
-  // index
   app.get("/", (req, res) => {
     return res.status(200).send({ message: "Welcome express mysql application." });
   });
-  // sign up new user
+
   router.post('/register', require('../Controllers/Auth.Controller.js').register);
-  // sign in (user authentication)
   router.post('/login', require('../Controllers/Auth.Controller.js').login);
-  // verify token (user authorization)
   router.get('/verify', require('../Controllers/Auth.Controller').userAuthorization);
-  // read all user data
   router.get('/users', require('../Controllers/User.Controller.js').findAll);
-  // read user data by id
   router.get('/users/:id', require('../Controllers/User.Controller.js').findOne);
-  // update user data by id
   router.put('/users/:id', require('../Controllers/User.Controller.js').findOneAndUpdate);
-  // delete user data by id
   router.delete('/users/:id', require('../Controllers/User.Controller.js').destroyById);
-  // delete all user data
   router.delete('/users', require('../Controllers/User.Controller.js').destroyAll);
 
   app.use('/api', router)
